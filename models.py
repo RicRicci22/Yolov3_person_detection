Big upgrade, solved bug on metrics calculation

models.py
@@ -380,7 +380,7 @@ class Yolov4Head(nn.Module):
 
 
 class Yolov4(nn.Module):
-    def __init__(self, yolov4conv137weight=None, n_classes=80):
+    def __init__(self, n_classes=80):
         super().__init__()
 
         output_ch = (4 + 1 + n_classes) * 3
@@ -393,17 +393,6 @@ class Yolov4(nn.Module):
         self.down5 = DownSample5()
         # neck
         self.neck = Neck()
-        # yolov4conv137
-        if yolov4conv137weight:
-            _model = nn.Sequential(self.down1, self.down2, self.down3, self.down4, self.down5, self.neck)
-            pretrained_dict = torch.load(yolov4conv137weight)
-
-            model_dict = _model.state_dict()
-            # 1. filter out unnecessary keys
-            pretrained_dict = {k1: v for (k, v), k1 in zip(pretrained_dict.items(), model_dict)}
-            # 2. overwrite entries in the existing state dict
-            model_dict.update(pretrained_dict)
-            _model.load_state_dict(model_dict)
         # head
         self.head = Yolov4Head(output_ch)
 
@@ -419,62 +408,14 @@ class Yolov4(nn.Module):
         output = self.head(x20, x13, x6)
         return output
 
-def process_videos_different_confidence(model, video_path, namesfile, confidences, num_classes):
-    # Script that automatically process the same 2 videos using different confidence thresholds
-    # confidences -> LIST of confidence values
-    import cv2
-    import fnmatch
-    # Break path into pieces
-    pieces = os.path.split(video_path)
-    for name, child in model.named_children():
-        print(name)
-    class_names = load_class_names(namesfile)
+    def load_weights(self,weightfile):
+        pretrained_dict = torch.load(weightfile,map_location=torch.device('cuda'))
+        self.load_state_dict(pretrained_dict)
+        return
 
-    f = open(pieces[0]+"/average_FPS.txt", "a")
-    f.write('\nFPS FOR DIFFERENT CONFIDENCE VALUES\n')
-
-    for threshold in confidences:
-        cap = cv2.VideoCapture(video_path)
-        # Get info on video
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        cap.set(3, 1280)
-        cap.set(4, 720)
-        print("Starting the YOLO loop...")
-        # Object to save the video
-        width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        writer= cv2.VideoWriter(pieces[0]+'/result_'+str(threshold)+'_'+pieces[1], 0x00000021, fps, (width,height))
-        n_frame = 1
-        start = time.time()
-        while (n_frame<=frame_count):
-            print('Processing frame',n_frame,'out of',frame_count)
-            ret, img = cap.read()
-            sized = cv2.resize(img, (1024, 1024))
-            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-
-            #start = time.time()
-            boxes = do_detect(model, sized, threshold, num_classes, 0.4, use_cuda)
-            #finish = time.time()
-            #print('Predicted in %f seconds.' % (finish - start))
-
-            result_img = plot_boxes_cv2(img, boxes, savename=None, class_names=class_names)
-
-            # cv2.imshow('Yolo demo', result_img)
-            writer.write(result_img)
-            cv2.waitKey(1)
-            n_frame=n_frame+1
-        finish = time.time()
-        # Printing average FPS in a txt file inside the folder
-        f.write(pieces[1]+'_confidence_'+str(threshold)+'-->'+str(round(frame_count/(finish-start),1))+'FPS\n')
-        f.close
-        cap.release()
-        writer.release()
-        # Compress video
-        print('Compressing video to 360p..')
-        compress_video(pieces[0]+'/result_'+str(threshold)+'_'+pieces[1],pieces[0]+'/result_'+str(threshold)+'_compressed'+pieces[1])
+    def activate_gpu(self):
+        self.cuda()
+        return
 
 
 if  __name__ == "__main__":
@@ -504,5 +445,5 @@ if  __name__ == "__main__":
     if use_cuda:
         model.cuda()
 
-    process_videos_different_confidence(model,videofile,[0.1,0.2,0.3,0.4],n_classes)
+    process_videos_different_confidence(model,videofile,namesfile,[0.1,0.2,0.3,0.4],n_classes)
 

tool/utils.py
@@ -105,104 +105,17 @@ def nms(boxes, nms_thresh):
             for j in range(i + 1, len(boxes)):
                 box_j = boxes[sortIds[j]]
                 if bbox_iou(box_i, box_j, x1y1x2y2=False) > nms_thresh:
-                    # print(box_i, box_j, bbox_iou(box_i, box_j, x1y1x2y2=False))
                     box_j[4] = 0
     return out_boxes
 
 
 def convert2cpu(gpu_matrix):
+    #return gpu_matrix.cpu()
     return torch.FloatTensor(gpu_matrix.size()).copy_(gpu_matrix)
 
 
 def convert2cpu_long(gpu_matrix):
     return torch.LongTensor(gpu_matrix.size()).copy_(gpu_matrix)
-
-
-def get_region_boxes_in_model(output, conf_thresh, num_classes, anchors, num_anchors, only_objectness=1,
-                              validation=False):
-    anchor_step = len(anchors) // num_anchors
-    if output.dim() == 3:
-        output = output.unsqueeze(0)
-    batch = output.size(0)
-    assert (output.size(1) == (5 + num_classes) * num_anchors)
-    h = output.size(2)
-    w = output.size(3)
-
-    t0 = time.time()
-    all_boxes = []
-    output = output.view(batch * num_anchors, 5 + num_classes, h * w).transpose(0, 1).contiguous().view(5 + num_classes,
-                                                                                                        batch * num_anchors * h * w)
-
-    grid_x = torch.linspace(0, w - 1, w).repeat(h, 1).repeat(batch * num_anchors, 1, 1).view(
-        batch * num_anchors * h * w).type_as(output)  # cuda()
-    grid_y = torch.linspace(0, h - 1, h).repeat(w, 1).t().repeat(batch * num_anchors, 1, 1).view(
-        batch * num_anchors * h * w).type_as(output)  # cuda()
-    xs = torch.sigmoid(output[0]) + grid_x
-    ys = torch.sigmoid(output[1]) + grid_y
-
-    anchor_w = torch.Tensor(anchors).view(num_anchors, anchor_step).index_select(1, torch.LongTensor([0]))
-    anchor_h = torch.Tensor(anchors).view(num_anchors, anchor_step).index_select(1, torch.LongTensor([1]))
-    anchor_w = anchor_w.repeat(batch, 1).repeat(1, 1, h * w).view(batch * num_anchors * h * w).type_as(output)  # cuda()
-    anchor_h = anchor_h.repeat(batch, 1).repeat(1, 1, h * w).view(batch * num_anchors * h * w).type_as(output)  # cuda()
-    ws = torch.exp(output[2]) * anchor_w
-    hs = torch.exp(output[3]) * anchor_h
-
-    det_confs = torch.sigmoid(output[4])
-
-    cls_confs = torch.nn.Softmax()(Variable(output[5:5 + num_classes].transpose(0, 1))).data
-    cls_max_confs, cls_max_ids = torch.max(cls_confs, 1)
-    cls_max_confs = cls_max_confs.view(-1)
-    cls_max_ids = cls_max_ids.view(-1)
-    t1 = time.time()
-
-    sz_hw = h * w
-    sz_hwa = sz_hw * num_anchors
-    det_confs = convert2cpu(det_confs)
-    cls_max_confs = convert2cpu(cls_max_confs)
-    cls_max_ids = convert2cpu_long(cls_max_ids)
-    xs = convert2cpu(xs)
-    ys = convert2cpu(ys)
-    ws = convert2cpu(ws)
-    hs = convert2cpu(hs)
-    if validation:
-        cls_confs = convert2cpu(cls_confs.view(-1, num_classes))
-    t2 = time.time()
-    for b in range(batch):
-        boxes = []
-        for cy in range(h):
-            for cx in range(w):
-                for i in range(num_anchors):
-                    ind = b * sz_hwa + i * sz_hw + cy * w + cx
-                    det_conf = det_confs[ind]
-                    if only_objectness:
-                        conf = det_confs[ind]
-                    else:
-                        conf = det_confs[ind] * cls_max_confs[ind]
-
-                    if conf > conf_thresh:
-                        bcx = xs[ind]
-                        bcy = ys[ind]
-                        bw = ws[ind]
-                        bh = hs[ind]
-                        cls_max_conf = cls_max_confs[ind]
-                        cls_max_id = cls_max_ids[ind]
-                        box = [bcx / w, bcy / h, bw / w, bh / h, det_conf, cls_max_conf, cls_max_id]
-                        if (not only_objectness) and validation:
-                            for c in range(num_classes):
-                                tmp_conf = cls_confs[ind][c]
-                                if c != cls_max_id and det_confs[ind] * tmp_conf > conf_thresh:
-                                    box.append(tmp_conf)
-                                    box.append(c)
-                        boxes.append(box)
-        all_boxes.append(boxes)
-    t3 = time.time()
-    if False:
-        print('---------------------------------')
-        print('matrix computation : %f' % (t1 - t0))
-        print('        gpu to cpu : %f' % (t2 - t1))
-        print('      boxes filter : %f' % (t3 - t2))
-        print('---------------------------------')
-    return all_boxes
 
 
 def get_region_boxes_out_model(output, conf_thresh, num_classes, anchors, num_anchors, only_objectness=1,
@@ -290,7 +203,7 @@ def get_region_boxes_out_model(output, conf_thresh, num_classes, anchors, num_an
 
 def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
     import cv2
-    colors = torch.FloatTensor([[1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0]]);
+    colors = torch.FloatTensor([[1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0]])
 
     def get_color(c, x, max_val):
         ratio = float(x) / max_val * 5
@@ -398,7 +311,6 @@ def load_class_names(namesfile):
 
 def do_detect(model, img, conf_thresh, n_classes, nms_thresh, use_cuda=1):
     model.eval()
-    t0 = time.time()
 
     if isinstance(img, Image.Image):
         width = img.width
@@ -415,12 +327,9 @@ def do_detect(model, img, conf_thresh, n_classes, nms_thresh, use_cuda=1):
         print("unknow image type")
         exit(-1)
 
-    t1 = time.time()
-
     if use_cuda:
         img = img.cuda()
     img = torch.autograd.Variable(img)
-    t2 = time.time()
 
     list_features = model(img)
 
@@ -450,23 +359,12 @@ def post_processing(img, conf_thresh, n_classes, nms_thresh, list_features_numpy
         bboxs_for_imgs = [
             boxes[0][index] + boxes[1][index] + boxes[2][index]
             for index in range(img.shape[0])]
-        # 分别对每一张图片的结果进行nms
-        t3 = time.time()
+
         boxes = [nms(bboxs, nms_thresh) for bboxs in bboxs_for_imgs]
     else:
         boxes = boxes[0][0] + boxes[1][0] + boxes[2][0]
-        t3 = time.time()
         boxes = nms(boxes, nms_thresh)
-    t4 = time.time()
 
-    if False:
-        print('-----------------------------------')
-        print(' image to tensor : %f' % (t1 - t0))
-        print('  tensor to cuda : %f' % (t2 - t1))
-        print('         predict : %f' % (t3 - t2))
-        print('             nms : %f' % (t4 - t3))
-        print('           total : %f' % (t4 - t0))
-        print('-----------------------------------')
     return boxes
 
 def compress_video(to_compress_path):